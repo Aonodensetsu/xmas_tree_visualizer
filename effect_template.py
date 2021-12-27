474c290ad9b0585cac02fb82f57abb341d9431ed
Aonodensetsu <58662350+Aonodensetsu@users.noreply.github.com>
2021-12-27 13:51:00 +0100

    Add files via upload

---
+++ effect_template.py
@@ -0,0 +1,20 @@
+def effect(storage, positions, frame):
+    #
+    # storage - you can write custom data into it, will be returned to you next frame
+    #
+    # positions - are given to you as a 2d array for each LED
+    # [ [x, y, z], [x, y, z], ...]
+    #
+    # frame - gives you the current frame number (starts at 1, ends at frame_max())
+    #
+    # return 2d array of normalized RGB values for each LED
+    # [ [r, g, b], [0.2, 0.7, 1], ... ]
+    #
+    rgb = [[0, 0, 0] for i in range(len(positions))]
+    return storage, rgb
+
+
+def frame_max():
+    # return the number of frames to render
+    # the visualizer runs at roughly 30 frames per second
+    return 1
